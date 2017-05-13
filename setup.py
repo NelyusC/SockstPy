6eca5d28059ff98346da63b99cf1fe296a50c790
NelyusC <nellolilo@gmail.com>
2017-05-14 02:56:47 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:         setup
+# Purpose:      Install SockstPy
+#
+#
+# Author:      Nethanel Coppenhagen
+#
+#-------------------------------------------------------------------------------
+
+
+from setuptools import setup
+
+
+setup(name = "SockstPy",
+      version = "1.0",
+      description = 'A Python package for SOCKS tunnelling.',
+      author = 'Nethanel Coppenhagen',
+      packages = ["sockstpy"])
